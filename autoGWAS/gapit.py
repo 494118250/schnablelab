e3c99ead9eb13b45588983409f202f67c6617994
cmiao@huskers.unl.edu <cmiao@huskers.unl.edu>
2018-03-05 14:52:42 -0600

    wrapper of cMLM in GAPIT

---
+++ autoGWAS/gapit.py
@@ -0,0 +1,60 @@
+# -*- coding: UTF-8 -*-
+
+"""
+Generate the R script file and the slurm job file for performing GAPIT. Find more details in GAPIT manual at <http://www.zzlab.net/GAPIT/gapit_help_document.pdf>
+"""
+
+import os.path as op
+import sys
+from JamesLab.apps.base import ActionDispatcher, OptionParser
+from JamesLab.apps.header import SlrumHeader
+from JamesLab.apps.header import Gapit_header
+from JamesLab.apps.natsort import natsorted
+
+def main():
+    actions = (
+        ('cMLM', 'Perform GWAS using compressed mixed linear model'),
+        ('SUPER', 'Perform GWAS using SUPER')
+            )
+    p = ActionDispatcher(actions)
+    p.dispatch(globals())
+
+def cMLM(args):
+    """
+    %prog cMLM pheno geno_prefix PCA Kinship
+    
+    Run automated GAPIT compressed mixed linear model
+    """
+    p = OptionParser(cMLM.__doc__)
+    p.set_slurm_opts(array=False)
+    opts, args = p.parse_args(args)
+
+    if len(args) == 0:
+        sys.exit(not p.print_help())
+    
+    pheno, geno_prefix, PCA, Kinship = args
+    mem = '.'.join(pheno.split('.')[0:-1])
+    f1 = open('%s.cMLM.R'%mem, 'w')
+    gapit_cmd = Gapit_header%(pheno,geno_prefix,geno_prefix,PCA,Kinship,mem)
+    f1.write(gapit_cmd)
+    
+    f2 = open('%s.cMLM.slurm'%mem, 'w')
+    h = SlrumHeader()
+    header = h.header%(opts.time, opts.memory, opts.prefix, opts.prefix, opts.prefix)
+    header.AddModule('R/3.3')
+    f2.write(header)
+    cmd = 'R CMD BATCH %s.cMLM.R'%mem
+    h2.write(cmd)
+    f1.close()
+    f2.close()
+    print('R script %s.cMLM.R and slurm file %s.cMLM.slurm has been created, you can sbatch your job file.'%(mem, mem))
+
+def SUPER(args):
+    """
+    %prog ...
+    Run automated GAPIT using SUPER
+    """
+    pass
+
+if __name__ == "__main__":
+    main()
