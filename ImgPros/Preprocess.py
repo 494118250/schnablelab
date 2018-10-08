cb82a694482c86d1f0290c725a3d37507cf4b8e1
cmiao@huskers.unl.edu <cmiao@huskers.unl.edu>
2018-10-07 22:26:23 -0500

    plant hull segmentation

--- ImgPros/Preprocess.py
+++ ImgPros/Preprocess.py
@@ -40,10 +40,14 @@ def PlantHull(args):
     count how many pixels belong to plant part
     """
     p = OptionParser(PlantHull.__doc__)
+    p.add_option('--thresh_cutoff', default= 150,
+                 help="set thresh cutt off in cv2.threshold function.")
     p.add_option('--border',
                  help="ignore image part out of boder: up,down,left,right")
     p.add_option('--crop',
                  help="crop image based on hull size")
+    p.add_option('--segmentation',
+                 help="convert background pixels to white.")
     opts, args = p.parse_args(args)
     if len(args) == 0:
         sys.exit(not p.print_help())
@@ -51,23 +55,37 @@ def PlantHull(args):
     img = cv2.imread(inputImg, 0)
     if opts.border:
         up, bot, lef, rig = [int(i) for i in opts.border.split(',')]
-        img[0:up, :] = 255
-        img[-bot:, :] = 255
-        img[:, 0:lef] = 255
-        img[:, -rig] = 255
-    _, thresh = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY)
-    thresh_ivt = invert(thresh)
-    chull = convex_hull_image(thresh_ivt)
+        if up !=0: img[0:up, :] = 255
+        if bot!=0: img[-bot:, :] = 255
+        if lef != 0: img[:, 0:lef] = 255
+        if rig != 0: img[:, -rig:] = 255
+    _, thresh = cv2.threshold(img, int(opts.thresh_cutoff), 255, cv2.THRESH_BINARY) # background is white(255), plant is black(0)
+    thresh_ivt = invert(thresh) # background to black(0), plant is white(255)
+    chull = convex_hull_image(thresh_ivt) # return False or True. Hull area is True
     pos = np.where(chull)
-    if opts.crop:
-        top, down, left, right = pos[0].min(), pos[0].max(), pos[1].min(), pos[1].max()
-        imgrgb = Image.open(inputImg)
-        imgrgb_crop = imgrgb.crop((left, top, right, down))
-        imgrgb_crop.save(inputImg.replace('.png', '.crp.png'))
+    #if opts.crop:
+    #    top, down, left, right = pos[0].min(), pos[0].max(), pos[1].min(), pos[1].max()
+    #    imgrgb = Image.open(inputImg)
+    #    imgrgb_crop = imgrgb.crop((left, top, right, down))
+    #    imgrgb_crop.save(inputImg.replace('.png', '.crp.png'))
+    if opts.segmentation:
+        myimgarr =cv2.imread(inputImg)
+        myimgarr = cv2.cvtColor(myimgarr, cv2.COLOR_BGR2RGB)
+        h_ivt3 = np.stack([thresh_ivt,thresh_ivt,thresh_ivt], 2)
+        segarr = np.where(h_ivt3 == 255, myimgarr, 255)
+        seg = Image.fromarray(segarr)
+        if opts.crop:
+            top, down, left, right = pos[0].min(), pos[0].max(), pos[1].min(), pos[1].max()
+            seg_crop = seg.crop((left, top, right, down))
+            seg_crop.save(inputImg.replace('.png', '.seg.crp.png'))
+        else:
+            seq.save(inputImg.replace('.png', '.seg.png'))      
     chull_diff = np.where(thresh_ivt == 255, 2, chull)
     misc.imsave('%s.hull.png' % (Path(inputImg).stem), chull_diff)
     PixelCount = np.sum(chull)
-    print('%s\t%s' % (inputImg, PixelCount))
+    fo = open('%s.PixelCounts'%Path(inputImg).stem, 'w')
+    fo.write('%s\t%s\n' %(Path(inputImg).name, PixelCount))
+    fo.close()
 
 
 def PlantHullBatch(args):
@@ -76,6 +94,8 @@ def PlantHullBatch(args):
     generate PlantHull jobs for all image files
     """
     p = OptionParser(PlantHullBatch.__doc__)
+    p.add_option('--mode', default='real', choices=['real', 'simu'],
+                 help="real image or simulated image.")
     p.set_slurm_opts()
     opts, args = p.parse_args(args)
     if len(args) == 0:
@@ -86,9 +106,12 @@ def PlantHullBatch(args):
     for img in all_imgs:
         imgpath = Path(img)
         outpre = str(imgpath.stem)
-        #cmd = 'python -m JamesLab.ImgPros.Preprocess PlantHull %s --border 80,10,10,0 > %s.ppnum\n' % (img, outpre)
-        cmd = 'python -m JamesLab.ImgPros.Preprocess PlantHull %s --crop True> %s.ppnum\n' % (img, outpre)
+        cmd = 'python -m JamesLab.ImgPros.Preprocess PlantHull %s --crop True --segmentation True --border 80,10,10,10\n' % (img) \
+            if opts.mode=='real' \
+            else 'python -m JamesLab.ImgPros.Preprocess PlantHull %s --crop True --segmentation True --border 0,40,10,0 --thresh_cutoff 180\n' % (img)
+        print(cmd)
         all_cmds.append(cmd)
+'''
     grps = cutlist(all_cmds, int(jobn))
     for gn, grp in grps:
         header = Slurm_header % (opts.time, opts.memory, outpre, outpre, outpre)
@@ -100,7 +123,7 @@ def PlantHullBatch(args):
         jobfile.write(header)
         jobfile.close()
         print('%s job file generated!' % jobname)
-
+'''
 
 def crop(args):
     """
