8519b45b96e9f8f8eef7f57b312587eb5c57167c
cmiao@huskers.unl.edu <cmiao@huskers.unl.edu>
2018-10-19 13:51:17 -0500

    request gpu and cpu terminals

--- HCCtools/RequestTerminal.py
+++ HCCtools/RequestTerminal.py
@@ -3,6 +3,7 @@ require personal terminal used for computation
 """
 import sys
 from JamesLab.apps.base import ActionDispatcher, OptionParser
+from subprocess import call
 
 def main():
 
@@ -19,16 +20,20 @@ def request_cpu(args):
     request a cpu node from hcc.
     """
     p = OptionParser(request_cpu.__doc__)
+    p.add_option("--partition", default="jclarke", choices=('batch', 'jclarke'),
+                help="which partition? [default: %default]")
     p.add_option("--memory", default="10240",
                 help="specify the how much memory [default: %default]")
     p.add_option("--time", default='20',
                 help = "specify the time (hour) [default: %default]")
     opts, args = p.parse_args(args)
-    if len(args) != 0:
+    if len(args) == 0:
+        print('add --help to see options.\n')
+        cmd = 'srun --partition=%s --mem-per-cpu=%s --ntasks-per-node=6 --nodes=1 --time=%s:0:0 --pty $SHELL\n'%(opts.partition, opts.memory, opts.time)
+        print(cmd)
+        call(cmd, shell=True)
+    else:
         sys.exit(not p.print_help())
-
-    cmd = 'srun --partition=jclarke --mem-per-cpu={memory} --ntasks-per-node=6 --nodes=1 --time={hour}:0:0 --pty $SHELL\n'.format(opts.memory, opts.time)
-    print(cmd)
 
 def request_gpu(args):
     """
@@ -36,18 +41,20 @@ def request_gpu(args):
     request a gpu node from hcc.
     """
     p = OptionParser(request_gpu.__doc__)
-    p.add_option("--memory", default="10000",
+    p.add_option("--memory", default="15000",
                 help="specify the how much memory [default: %default]")
     p.add_option("--time", default='20',
                 help = "specify the time (hour) [default: %default]")
-    p.add_option("--model", default='p100', choices=(gpu_p100, gpu_k20, gpu_k40),
+    p.add_option("--model", default='gpu_p100', choices=('gpu_p100', 'gpu_k20', 'gpu_k40'),
                 help = "specify gpu mode, p100:16gb, k40:12gb, k20:5bg [default: %default]")
     opts, args = p.parse_args(args)
-    if len(args) != 0:
+    if len(args) == 0:
+        print('add --help to see options.\n')
+        cmd = 'srun --partition=gpu --gres=gpu --constraint=%s --mem-per-cpu=%s --ntasks-per-node=1 --nodes=1 --time=%s:0:0 --pty $SHELL\n'%(opts.model, opts.memory, opts.time)
+        print(cmd)
+        call(cmd, shell=True)
+    else:
         sys.exit(not p.print_help())
-
-    cmd = 'srun --partition=gpu --gres=gpu --constraint={model} --mem-per-cpu={memory} --ntasks-per-node=1 --nodes=1 --time={hour}:0:0 --pty $SHELL\n'.format(opts.model, opts.memory, opts.time)
-    print(cmd)
 
 if __name__ == "__main__":
     main()
