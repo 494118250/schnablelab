209c58819d350f043fbde385164159804320e9fe
Alex Pages <alejandro.pages@huskers.unl.edu>
2018-11-28 21:55:52 -0600

    Added zooniverse management script

--- ImgPros/zooniverse.py
+++ ImgPros/zooniverse.py
@@ -3,14 +3,22 @@
 """
 Upload images to zooniverse project
 """
-
+from getpass import getuser, getpass
 import importlib
 from JamesLab.apps.base import ActionDispatcher, OptionParser
+import pickle as pkl
+import sys
+import os
+import traceback as tb
+from glob import glob
 
-pc = importlib.util.find_spec("panoptes_client")
-if pc is None:
+
+try:
+    from panoptes_client import panoptes, Panoptes, user, Project, SubjectSet, Subject
+except ImportError:
     print("panoptes_client package could not be imported. To install use:")
     print("> pip install panoptes-client")
+    print("Or activate your panoptes-client enabled anaconda environment")
     quit()
 
 
@@ -25,14 +33,14 @@ def main():
 def upload(args):
     """
     %prog upload img_directory zoo_project_id --subject subject_setID
-    Uploads images from img_directory to zooniverse project with zoo_project_id.
-    Can designate a subject set id or create a new one.
+
+    Desc: Upload images from img_directory to zooniverse project with zoo_project_id.
     """
     p = OptionParser(upload.__doc__)
     p.add_option('--subject', default=False,
-        help = 'Designate a subject set id if subect set already exists.\
-                Default is a new subject set will be created.')
-
+        help = 'Designate a subject set id')
+    p.add_option('--manifest', default='manifest.csv',
+        help = 'Optionally specify a manifest filename')
     opts, args = p.parse_args(args)
 
     if len(args) == 0:
@@ -40,7 +48,116 @@ def upload(args):
 
     img_dir, proj_id = args
 
+    if not os.path.isdir(img_dir):
+        print("{} is not a valid path".format(img_dir))
+        return
 
+    un, pw, sc = __get_zoo_credentials()
+
+    try:
+        Panoptes.connect(username=un, password=pw)
+        project = Project.find(id = proj_id)
+        if opts.subject:
+            subject = SubjectSet.find(opts.subject)
+        else:
+            subject_set = SubjectSet()
+            subject_set.links.project = project
+            dispname = input("Enter subject set display name: ")
+            subject_set.display_name(dispname)
+            subject_set.save()
+            project.reload()
+
+        if sc: __save_creds(un, pw)
+
+    except Exception as e:
+        tb.print_exc()
+
+    if not os.path.isfile(os.path.join(img_dir, opts.manifest)):
+        while True:
+            print("There is no manifest, generating...")
+                __generate_manifest(img_dir)
+
+
+def __generate_manifest(img_dir):
+    # TODO: get all files that are png or jpg
+
+
+def get_export(args):
+    """
+    %prog get_export project_id
+
+    Get classification or other export
+    """
+    p = OptionParser(upload.__doc__)
+    p.add_option('--type', default='classification'
+                help='specify the type of export')
+    opts, args = p.parse_args(args)
+
+    if len(args) == 0:
+        sys.exit(not p.print_help())
+
+    un, pw, sc = __get_zoo_credentials()
+
+    try:
+        Panoptes.connect(username=un, password=pw)
+        project = Project.find(id = proj_id)
+        project
+
+
+def __get_zoo_credentials():
+    if os.path.exists("./.zoo_creds.pkl"):
+        if input("Used saved login credentials? (y/n)") == 'y':
+            with open("./.zoo_creds.pkl", 'rb') as usersfile:
+                users = pkl.load(usersfile)
+                print(users)
+                if not users:
+                    return __get_new_creds()
+                print("Available users credentials: ")
+                print("{:3d} - {}".format(0, 'other'))
+                for i, user in enumerate(list(users.keys())):
+                    print("{:3d} - {}".format(i+1, user))
+                while True:
+                    ind = input("Which username? ")
+                    if not ind.isdigit(): continue
+                    if int(ind) < 0 or int(ind) > len(users.keys()):
+                        print("Invalid Entry")
+                    elif ind == '0':
+                        return __get_new_creds()
+                    else:
+                        un = list(users.keys())[int(ind)-1]
+                        pw = users[un]
+                        return un, pw, None
+
+    return __get_new_creds()
+
+
+def __get_new_creds():
+
+    un = input("Enter zooniverse username: ")
+    pw = getpass()
+
+    if input("Would you like to save your credentials if valid? (y/n) ") in ['y', 'Y']:
+        sc=True
+    else:
+        sc=False
+
+    return un, pw, sc
+
+
+def __save_creds(un, pw):
+
+    print("Saving credentials")
+    if os.path.exists(".zoo_creds.pkl"):
+        with open(".zoo_creds.pkl", 'rb') as pfile:
+            creds = pkl.load(pfile)
+            creds[un] = pw
+    else:
+        creds = {un:pw}
+
+    with open(".zoo_creds.pkl", 'wb') as pfile:
+        pkl.dump(creds, pfile)
+
+    return None
 
 
 if __name__ == '__main__':
