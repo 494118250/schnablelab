5995dd92c213d533c46ab8a6595be444ffea468f
Alex Pages <alejandro.pages@huskers.unl.edu>
2018-12-08 21:43:16 -0600

    Simplified the credential getting. Refactored a bit.

--- ImgPros/tests/test_zookeeper.py
+++ ImgPros/tests/test_zookeeper.py
@@ -14,23 +14,48 @@ from io import StringIO
 
 
 
+
 def test___generate_manifest():
     img_dir = osp.join(osp.dirname(__file__), 'data', 'imgs')
     mfile = zookeeper.__generate_manifest(img_dir)
     assert osp.exists(osp.join(img_dir, 'manifest.csv'))
 
     PATTERN = re.compile(".*\.(jpg|png)")
-    mfile.seek(0)
     assert sum(1 for i in mfile.readlines()) - 1 == len([i for i in os.listdir(img_dir) if PATTERN.match(i)])
 
 
 def test_upload():
 
+    cred = {'un': 'alejandropages', 'pw': '12Buckl3mYSh03'}
+
     img_dir = osp.join(osp.dirname(__file__), 'data', 'imgs')
+    img_no_man = osp.join(osp.dirname(__file__), 'data', 'imgs_no_manifest')
     proj_id = '7802'
 
-    with pytest.raises(pan.panoptes.PanoptesAPIException):
-        zookeeper.upload([img_dir, proj_id], 'fubar', 'chicken')
+    with pytest.raises(SystemExit) as e:
+        zookeeper.upload(['DNE', proj_id])
+
+    assert e.type == SystemExit
+    print("VALUE FROM SYSTEM EXIT: {}".format(e.value.code))
+    quit()
+    assert e.value.code == -1
+
 
     with pytest.raises(pan.panoptes.PanoptesAPIException):
-        zookeeper.upload([img_dir, proj_id], 'alejandropages', '12Buckl3mYSh03', dispname='test_subject')
+        zookeeper.upload([img_dir, proj_id],
+                         'fubar', 'chicken')
+
+    with pytest.raises(pan.panoptes.PanoptesAPIException):
+        zookeeper.upload([img_dir, proj_id],
+                         cred.un, cred.pw,
+                         dispname='test_subject')
+
+    with pytest.raises(pan.panoptes.PanoptesAPIException):
+        zookeeper.upload([img_dir, proj_id],
+                         cred.un, cred.pw,
+                         subjsetid=567)
+
+    with pytest.raises(pan.panoptes.PanoptesAPIException):
+        zookeeper.upload([img_no_man, proj_id],
+                         cred.un, cred.pw,
+                         subjsetid=67200)
--- ImgPros/zookeeper.py
+++ ImgPros/zookeeper.py
@@ -7,20 +7,23 @@ from getpass import getpass
 from JamesLab.apps.base import ActionDispatcher, OptionParser
 import pickle as pkl
 import sys
+import os.path as osp
 import os
-from glob import glob
 from datetime import datetime as dt
 import csv
 import logging
-import json
 from pprint import pprint
 import re
 
 
+'''
+New concepts:
+'''
 log=logging.getLogger('zookeeper')
 log.setLevel(logging.DEBUG)
 log.addHandler(logging.FileHandler('zoo_load.err'))
 log.addHandler(logging.StreamHandler())
+
 
 try:
     import panoptes_client as pan
@@ -35,25 +38,19 @@ def main():
     actions = (
         ('upload', 'load images to zooniverse'),
         ('export', 'Get annotation and other exports'),
-        ('retire', 'Retire subjects that have been'),
         ('manifest', 'Generate a manifest for zooniverse subject set upload')
     )
     p = ActionDispatcher(actions)
     p.dispatch(globals())
 
 
-def retire(args):
-    raise NotImplementedError
-
-
 def manifest(args):
-    p = OptionParser(manifest.__doc__, usage="Generate a manifest for "
-        + "zooniverse subject set upload.")
+    p = OptionParser(manifest.__doc__, usage="Generate a manifest for"
+        + " zooniverse subject set upload.")
     raise NotImplementedError
 
 
-# UNTESTED
-def upload(args, un=None, pw=None, dispname=False):
+def upload(args, un=None, pw=None, dispname=None, subjsetid=None):
     """
     %prog upload img_directory zoo_project_id
     """
@@ -79,63 +76,66 @@ def upload(args, un=None, pw=None, dispname=False):
     if opts.quiet:
         log.setLevel(logging.ERROR)
 
-    if not os.path.isdir(img_dir):
-        log.info("{} is not a valid path".format(img_dir))
-        return
-
-    if not un and not pw:
-        un, pw, sc = __get_zoo_credentials()
-    else:
-        sc = False
+    if not osp.isdir(img_dir):
+        log.error("{} is not a valid path".format(img_dir))
+        quit()
+        
+    un, pw = __get_zoo_creds()
 
     try:
-        log.info("Logging in as {}".format(un))
+        log.info("Logging in...")
         pan.Panoptes.connect(username=un, password=pw)
-        pprint(pan.user.User())
-
-        if sc:
-            __save_creds(un, pw)
-
-        subjdat = None
-
         project = pan.Project.find(id=proj_id)
-        if opts.subject:
-            subject_set = pan.SubjectSet.find(opts.subject)
-        else:
-            log.info("Creating new subject set")
-            subject_set = pan.SubjectSet()
-            print(subject_set)
-            subject_set.links.project = project
-            if not dispname:
-                dispname = input("Enter subject set display name: ")
-            try:
-                subject_set.display_name = dispname
-                subjdat = subject_set.save()
-
-            except pan.panoptes.PanoptesAPIException as e:
-                log.info("Subject set display name already taken")
-                if not dispname:
-                    quit()
-                else:
-                    raise
-
-
     except pan.panoptes.PanoptesAPIException as e:
+        log.error("Either the credentials were wrong or"
+                  + " the project id does not exist:")
         log.error("PanoptesAPIException: {}".format(e))
         raise
 
-    if not os.path.isfile(os.path.join(img_dir, 'manifest.csv')):
-        log.info("There is no manifest, one will be generated.")
-        with __generate_manifest(img_dir) as mfile:
-            reader = csv.DictReader(mfile)
+    if not opts.subject and not subjsetid:
+        log.info("Creating new subject set")
+        subject_set = pan.SubjectSet()
+        subject_set.links.project = project
+
+        if not dispname:
+            dispname = input("Enter subject set display name: ")
+
+        try:
+            subject_set.display_name = dispname
+            subject_set.save()
+
+        except pan.panoptes.PanoptesAPIException as e:
+            log.error("Subject set display name already taken")
+            if not dispname:
+                quit()
+            raise
 
     else:
-        with open(os.path.join(img_dir, 'manifest.csv'), 'r') as mfile:
+        try:
+            if opts.subject:
+                subject_set = pan.SubjectSet.find(opts.subject)
+            else:
+                subject_set = pan.SubjectSet.find(subjsetid)
+        except pan.panoptes.PanoptesAPIException as e:
+            log.error("Could not find subject set id")
+            raise
+
+    if osp.isfile(osp.join(img_dir, 'manifest.csv')):
+        with open(osp.join(img_dir, 'manifest.csv'), 'r') as mfile:
             reader = csv.DictReader(mfile)
+    else:
+        log.info("There is no manifest, one will be generated.")
+        mfile = __generate_manifest(img_dir)
+        mfile.close()
+
+        ''' Would this work?
+        with __generate_manifest(img_dir) as mfile:
+            reader = csv.DictReader(mfile)
+        '''
 
     fieldnames = reader.fieldnames()
     if 'filename' not in fieldnames:
-        log.info("Manifest file must have a filename field")
+        log.info("Manifest file must have a 'filename' column")
         quit()
 
     project.reload()
@@ -161,7 +161,39 @@ def upload(args, un=None, pw=None, dispname=False):
         pan.workflow.Workflow().find(opts.workflow)
     else:
         log.info("Because you did not specify a workflow, you will need to link"
-            + " your subject set to a workflow.")
+                 + " your subject set to a workflow.")
+
+
+def __generate_manifest(img_dir):
+
+    log.info("Manifest being generated with fields: [ id, filename ]")
+
+    # get all files that are png or jpg
+    idtag = dt.now().strftime("%m%d%y-%H%M%S")
+
+    img_count = 0
+
+    mfile = open(osp.join(img_dir, 'manifest.csv'), 'w+')
+
+    writer = csv.writer(mfile)
+    writer.writerow(["id", "filename"])
+
+    PATTERN = re.compile(".*\.(jpg|png|tiff)")
+
+    for id, filename in enumerate(os.listdir(img_dir)):
+        if PATTERN.match(filename):
+            writer.writerow(["{}-{:04d}".format(idtag, id), filename])
+            img_count += 1
+        if img_count == 9999:
+            log.warning("Zooniverse's default limit for subjects per user is"
+                        + " 9999. You may not be able to load the entire "
+                        + " subject set.")
+            if not __get_yn("Continue?"):
+                break
+            img_count += 1
+
+    log.info("{} subjects written to manifest".format(img_count))
+    return mfile.seek(0)
 
 # UNTESTED
 def export(args):
@@ -181,11 +213,11 @@ def export(args):
 
     proj_id, output_dir = args
 
-    if not os.path.isdir(output_dir):
+    if not osp.isdir(output_dir):
         log.info("Output directory does not exist")
         quit()
 
-    un, pw, sc = __get_zoo_credentials()
+    un, pw = __get_zoo_creds()
 
     try:
         pan.Panoptes.connect(username=un, password=pw)
@@ -197,7 +229,7 @@ def export(args):
     try:
         log.info("Getting export, this may take some time.")
         export = project.get_export(opts.type)
-        with open(os.path.join(output_dir, \
+        with open(osp.join(output_dir, \
             "zoo_export.{}.csv".format(dt.now().strftime('%b-%d-%G-%I%:M%p')),
             'w')) as zoof:
             zoof.write(export)
@@ -205,40 +237,46 @@ def export(args):
         log.error("Error getting export: {}".format(e))
         raise
 
-# UNTESTED
-def __generate_manifest(img_dir):
 
-    log.info("Manifest being generated with fields: [ id, filename ]")
 
-    # get all files that are png or jpg
-    idtag = dt.now().strftime("%m%d%y-%H%M%S")
-    mfile = open(os.path.join(img_dir, 'manifest.csv'), 'w+')
 
-    count = 0
-    writer = csv.writer(mfile)
-    writer.writerow(["id","filename"])
+def __get_yn(message):
+    while True:
+        val = input(message + " [y/n]")
+        if val.lower() in ['y', 'n']:
+            return True if val.lower() == 'y' else False
+        else:
+            print("Invalid input")
 
-    PATTERN = re.compile(".*\.(jpg|png)")
 
-    for id, filename in enumerate(os.listdir(img_dir)):
-        if PATTERN.match(filename):
-            writer.writerow(["{}-{:04d}".format(idtag, id), filename])
-            count += 1
-        if count >= 9999:
-            log.info("This upload is too big, limit is 9999 images.")
-            break
+def __get_zoo_creds():
 
-    log.info("Manifest generated for {} files.".format(count))
+    un = input("Enter zooniverse username: ")
+    pw = getpass()
 
-    return mfile
+    return un, pw
 
-"""
-Private functions:
-"""
 
-# UNTESTED
+### GOLD PLATING FEATURE DUMP ###
+'''
+DECIDED THAT ZOONIVERSE WRAPPER WASN'T A GOOD PLACE TO IMPLEMENT
+FULL FEATURED CREDENTIAL MANAGEMENT SOFTWARE
+
+def __save_creds(un, pw):
+
+    log.info("Saving credentials")
+
+    if osp.exists(osp.join(osp.expanduser("~"), ".zoo")):
+        with open(osp.join(osp.expanduser("~"), ".zoo", 'rb+')) as pfile:
+            creds = json.load(pfile)
+            creds[un] = pw
+            json.dump(creds, pfile)
+    else:
+        with open(osp.join(osp.expanduser("~"), ".zoo", 'wb')) as pfile:
+            json.dump(creds, pfile)
+
 def __get_zoo_credentials():
-    if not os.path.exists("./.zoo.pkl"):
+    if not osp.exists("./.zoo.pkl"):
         return __get_new_user_creds()
     elif input("Used saved login credentials? (y/n)") == 'y':
         with open("./.zoo.pkl", 'rb') as usersfile:
@@ -265,35 +303,7 @@ def __get_zoo_credentials():
                     return un, pw, None
 
     return __get_new_user_creds()
-
-
-def __get_new_user_creds():
-
-    un = input("Enter zooniverse username: ")
-    pw = getpass()
-
-    if input("Would you like to save your credentials if valid? (y/n) ") \
-        in ['y', 'Y']:
-        sc = True
-    else:
-        sc = False
-
-    return un, pw, sc
-
-
-def __save_creds(un, pw):
-
-    log.info("Saving credentials")
-
-    if os.path.exists(os.path.join(os.path.expanduser("~"), ".zoo")):
-        with open(os.path.join(os.path.expanduser("~"), ".zoo", 'rb+')) as pfile:
-            creds = json.load(pfile)
-            creds[un] = pw
-            json.dump(creds, pfile)
-    else:
-        with open(os.path.join(os.path.expanduser("~"), ".zoo", 'wb')) as pfile:
-            json.dump(creds, pfile)
-
+'''
 
 if __name__ == '__main__':
     main()
