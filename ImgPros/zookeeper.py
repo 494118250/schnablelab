1d0477cd96b08a2d627f87f8359dd647dc853cbf
alpacapages <alpacapages@gmail.com>
2018-12-11 21:31:53 -0600

    Added docstrings to all functions. Also trying to figure out the FileNotFoundError and exception handling in python

--- ImgPros/zookeeper.py
+++ ImgPros/zookeeper.py
@@ -5,21 +5,21 @@ Upload images to zooniverse project
 """
 from getpass import getpass
 from JamesLab.apps.base import ActionDispatcher, OptionParser
-import pickle as pkl
 import sys
 import os.path as osp
 import os
+import errno
 from datetime import datetime as dt
 import csv
 import logging
-from pprint import pprint
 import re
+import OSError
 
 
 '''
-New concepts:
+Set logging objects
 '''
-log=logging.getLogger('zookeeper')
+log = logging.getLogger('zookeeper')
 log.setLevel(logging.DEBUG)
 log.addHandler(logging.FileHandler('zoo_load.err'))
 log.addHandler(logging.StreamHandler())
@@ -30,7 +30,7 @@ try:
 except ImportError:
     log.info("panoptes_client package could not be imported. To install use:")
     log.info("> pip install panoptes-client")
-    log.info("Or activate your panoptes-client enabled anaconda environment")
+    log.info("Or activate your panoptes-client enabled environment")
     quit()
 
 
@@ -45,52 +45,95 @@ def main():
 
 
 def manifest(args):
-    p = OptionParser(manifest.__doc__, usage="Generate a manifest for"
-        + " zooniverse subject set upload.")
-    raise NotImplementedError
+    '''
+    %prog manifest img_directory
 
+    Does:
+        Generates a manifest inside the specified image directory.
 
-def upload(args, un=None, pw=None, dispname=None, subjsetid=None):
-    """
-    %prog upload img_directory zoo_project_id
-    """
-    p = OptionParser(upload.__doc__)
-    p.add_option('-s', '--subject', default=False,
-        help='Designate a subject set id')
-    p.add_option('-q', '--quiet', default=False,
-        help='Silences output when uploading images to zooniverse.')
-    p.add_option('-w', '--workflow', default=None,
-        help='Designate a workflow id to link to this subject set')
-    # TODO: ??
-    p.add_option('-c', '--convert', default=False,
-        help='Compress and convert files to jpg for faster load times on zooniverse.\n'\
-            +'Command: magick -strip -interlace Plane -quality 85% -format jpg <img_directory>/<filename>.png')
+    Args:
+        ~: img_dir
+            -type: str
+            -desc: The image directory in which to generate the manifest.
+    Returns:
+        None
+    '''
+    p = OptionParser(manifest.__doc__)
 
     opts, args = p.parse_args(args)
 
-    if len(args) == 0:
-        sys.exit(not p.print_help())
+    if len(sys.argv) != 1:
+        exit(not p.print_help())
+
+    img_dir = args[1]
+
+    try:
+        __generate_manifest(img_dir, return_handle=False)
+    except OSError as e:
+        if e.errno == errno.ERNOENT:
+            log.error("Image directory did not exist")
+        else:
+            lor.error("Could not generate manifest file: {}".format(e))
+        exit(-1)
+
+
+def upload(args, un=None, pw=None, dispname=None, subjsetid=None):
+    '''
+    %prog upload img_dir zoo_proj_id
+
+    Does:
+        Uploads images from the specified image directory to zooniverse
+        project specified by zoo_project_id. This function will also
+        generate a manifest if one is not already present inside img_dir.
+
+    Args:
+        ~: img_dir
+            -type: str
+            -desc: The directory of the images to be uploaded
+        ~: proj_id
+            -type: str
+            -desc: The zooniverse project id to upload the images to.
+    Returns:
+        None
+
+    '''
+    p = OptionParser(upload.__doc__)
+    p.add_option('-s', '--subject', default=False,
+                 help='Designate a subject set id')
+    p.add_option('-q', '--quiet', default=False,
+                 help='Silences output when uploading images to zooniverse.')
+    p.add_option('-w', '--workflow', default=None,
+                 help='Designate a workflow id to link to this subject set')
+    # TODO: ??
+    p.add_option('-c', '--convert', default=False,
+                 help="Compress and convert files to jpg for faster load times"
+                 + " on zooniverse.\n"
+                 + " Command: magick -strip -interlace Plane -quality 85%"
+                 + " -format jpg <img_directory>/<filename>.png")
+
+    opts, args = p.parse_args(args)
+
+    if len(args) != 2:
+        exit(not p.print_help())
 
     img_dir, proj_id = args
 
     if opts.quiet:
         log.setLevel(logging.ERROR)
 
-    if not osp.isdir(img_dir):
-        log.error("{} is not a valid path".format(img_dir))
-        quit()
-        
-    un, pw = __get_zoo_creds()
+    try:
+        __check_dir(img_dir)
+    except OSError as e:
+        if e.errno == errno.ENOENT:
+            log.error("Could not find image directory")
+            exit(-1)
+        else:
+            log.error("Error checking image directory: {}".format(e))
 
     try:
-        log.info("Logging in...")
-        pan.Panoptes.connect(username=un, password=pw)
-        project = pan.Project.find(id=proj_id)
-    except pan.panoptes.PanoptesAPIException as e:
-        log.error("Either the credentials were wrong or"
-                  + " the project id does not exist:")
-        log.error("PanoptesAPIException: {}".format(e))
-        raise
+        project = __connect_zoo(proj_id)
+    except pan.panoptes.PanoptesAPIException:
+        exit(-1)
 
     if not opts.subject and not subjsetid:
         log.info("Creating new subject set")
@@ -136,7 +179,7 @@ def upload(args, un=None, pw=None, dispname=None, subjsetid=None):
     fieldnames = reader.fieldnames()
     if 'filename' not in fieldnames:
         log.info("Manifest file must have a 'filename' column")
-        quit()
+        exit(-1)
 
     project.reload()
 
@@ -148,7 +191,7 @@ def upload(args, un=None, pw=None, dispname=None, subjsetid=None):
         try:
             curr_subj = pan.Subject()
             curr_subj.add_location(row['filename'])
-            curr_subj.metadate.update(row)
+            curr_subj.metadata.update(row)
             curr_subj.save()
             subject_set.add(curr_subj)
         except pan.panoptes.PanoptesAPIException as e:
@@ -160,84 +203,156 @@ def upload(args, un=None, pw=None, dispname=None, subjsetid=None):
     if opts.workflow:
         pan.workflow.Workflow().find(opts.workflow)
     else:
-        log.info("Because you did not specify a workflow, you will need to link"
-                 + " your subject set to a workflow.")
+        log.info("Because you did not specify a workflow, you will need to"
+                 + " link your subject set to a workflow.")
 
 
-def __generate_manifest(img_dir):
+# UNTESTED
+def export(args):
+    '''
+    %prog export project_id output_dir
+
+    Does:
+        Fetches export from zooniverse for specified project.
+
+    Args:
+        ~: project_id
+            -type: str
+            -desc: The zooniverse project id
+        ~: output_dir
+            -type: str
+            -desc: Path to the image directory with images to be uploaded
+    Returns:
+        None
+    '''
+    p = OptionParser(upload.__doc__)
+    p.add_option('-t', '--type', default='classifications',
+                 help='specify the type of export')
+
+    opts, args = p.parse_args(args)
+
+    if len(args) != 2:
+        exit(not p.print_help())
+
+    proj_id, output_dir = args
+
+    if not osp.isdir(output_dir):
+        log.error("Output directory does not exist")
+        raise OSError("{} does not exist".format(output_dir))
+
+    project = __connect_zoo(proj_id)
+
+    try:
+        log.info("Getting export, this may take some time.")
+        export = project.get_export(opts.type)
+        fname = "zoo_export.{}.csv".format(
+                dt.now().strftime('%b-%d-%G-%I%:M%p'))
+        with open(osp.join(output_dir, fname), 'w') as zoof:
+            zoof.write(export)
+    except pan.panoptes.PanoptesAPIException as e:
+        log.error("Error getting export: {}".format(e.args))
+        exit(-1)
+
+
+'''
+HELPER FUNCTIONS
+'''
+
+
+def __check_dir(dir):
+    if osp.exists(dir):
+        return True
+    log.error("Directory {} does not exist".format(dir))
+    return False
+
+def __generate_manifest(img_dir, return_handle=True):
+    '''
+    Does:
+        Generates a generic manifest in the specified image directory.
+        Fields are an id in the format [date]-[time]-[filenumber] and
+        the filename.
+
+    Args
+        ~: img_dir
+            -type: str
+            -desc: image directory for which to generate manifest
+        ~: return_handle
+            -type: bool
+            -desc: Whether to return open file handle to the new manifest
+    Returns:
+        If return_handle is True
+        --> file handle to new manifest
+        Else
+        --> None
+
+    Notes:
+        Supported image types: [ tiff, jpg, png ] - can easily append
+    '''
+
+    __check_dir(img_dir)
+
 
     log.info("Manifest being generated with fields: [ id, filename ]")
 
-    # get all files that are png or jpg
-    idtag = dt.now().strftime("%m%d%y-%H%M%S")
-
-    img_count = 0
-
     mfile = open(osp.join(img_dir, 'manifest.csv'), 'w+')
-
+    idtag = dt.now().strftime("%m%d%y-%H%M%S")
     writer = csv.writer(mfile)
     writer.writerow(["id", "filename"])
 
-    PATTERN = re.compile(".*\.(jpg|png|tiff)")
+    # Insert "|<img_file_pattern>" for additional image types
+    PATTERN = re.compile(r".*\.(jpg|png|tiff)")
+    img_count = 0
 
     for id, filename in enumerate(os.listdir(img_dir)):
         if PATTERN.match(filename):
             writer.writerow(["{}-{:04d}".format(idtag, id), filename])
             img_count += 1
         if img_count == 9999:
-            log.warning("Zooniverse's default limit for subjects per user is"
-                        + " 9999. You may not be able to load the entire "
+            log.warning("Zooniverse's default limit of subjects per user is"
+                        + " 9999. You may not be able to load this entire"
                         + " subject set.")
             if not __get_yn("Continue?"):
                 break
             img_count += 1
 
     log.info("{} subjects written to manifest".format(img_count))
-    return mfile.seek(0)
 
-# UNTESTED
-def export(args):
-    """
-    %prog export project_id output_dir
+    if return_handle:
+        return mfile.seek(0)
+    else:
+        mfile.close()
+        return None
 
-    Get classifications or other export
-    """
-    p = OptionParser(upload.__doc__)
-    p.add_option('-t', '--type', default='classifications',
-                help='specify the type of export')
 
-    opts, args = p.parse_args(args)
+def __connect_zoo(proj_id, un=None, pw=None):
+    '''
+    Does:
+        Connects to the zooniverse through the panoptes api
+        and gets an instance of the project.
 
-    if len(args) == 0:
-        sys.exit(not p.print_help())
+    Args:
+        ~: proj_id
+            -type: str
+            -desc: The zooniverse project id
+    Returns:
+        ~:proj
+            -type: obj - panoptes_client.Project.project
+            -desc: and instance of the zooniverse project
 
-    proj_id, output_dir = args
-
-    if not osp.isdir(output_dir):
-        log.info("Output directory does not exist")
-        quit()
-
-    un, pw = __get_zoo_creds()
+    Raises:
+        PanoptesAPIException
+    '''
+    un = input("Enter zooniverse username: ")
+    pw = getpass()
 
     try:
         pan.Panoptes.connect(username=un, password=pw)
-        project = pan.Project.find(id = proj_id)
-        pprint(pan.user.User())
+        project = pan.Project.find(id=proj_id)
     except pan.panoptes.PanoptesAPIException as e:
-        log.error("Could not connect: {}".format(e))
+        log.error("Could not complete connection to zooniverse: {}"
+                  .format(e.args))
         raise
-    try:
-        log.info("Getting export, this may take some time.")
-        export = project.get_export(opts.type)
-        with open(osp.join(output_dir, \
-            "zoo_export.{}.csv".format(dt.now().strftime('%b-%d-%G-%I%:M%p')),
-            'w')) as zoof:
-            zoof.write(export)
-    except pan.panoptes.PanoptesAPIException as e:
-        log.error("Error getting export: {}".format(e))
-        raise
-
-
+    return project
 
 
 def __get_yn(message):
@@ -247,14 +362,6 @@ def __get_yn(message):
             return True if val.lower() == 'y' else False
         else:
             print("Invalid input")
-
-
-def __get_zoo_creds():
-
-    un = input("Enter zooniverse username: ")
-    pw = getpass()
-
-    return un, pw
 
 
 ### GOLD PLATING FEATURE DUMP ###
